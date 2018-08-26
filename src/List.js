288c5d254ad3fa5ff2a82acb032fb7c5084b2345
walaa_ayyad <walaa_ayyad7@yahoo.com>
2018-08-26 02:57:38 +0200

    upload changes

--- src/List.js
+++ src/List.js
@@ -6,6 +6,7 @@ import sortBy from 'sort-by'
 class List extends Component {
   state={
       query:'',
+      locations:[],
       markers: this.props.markers
   }
 
@@ -19,17 +20,11 @@ class List extends Component {
             //console.log(this.state.markers)
             //console.log(this.props.locations)
            let showingRestaurant
-           let filteredMarkers
            if(this.state.query) {
                const match = new RegExp(escapeRegExp(this.state.query), 'i')
-            //--test part to filter markers
-            filteredMarkers= this.state.markers.filter((restaurant)=> match.test(restaurant.title))
-            console.log(filteredMarkers)
-            //---------------------
             showingRestaurant = this.props.locations.filter((restaurant)=> match.test(restaurant.name))
             //console.log(showingRestaurant)
-            
-          
+                
            }else{
             showingRestaurant = this.props.locations
            }
@@ -43,11 +38,12 @@ class List extends Component {
                     <div className='search-list'>
                         <input className='search'
                             role='search'
-                            aria-labelleby='search_field'
+                            aria-labelledby='search_field'
                             type='text'
                             placeholder='Search Restaurant'
+                            onChange={(event)=>{this.updateQuery(event.target.value);
+                                     this.props.onUpdateQuery(event.target.value)}} 
                             value={this.state.query}
-                            onChange={(event)=>this.updateQuery(event.target.value) && this.props.updateMarkers(filteredMarkers)}//--test part to filter markers
                         />
                     <h5 tabIndex='0'>Click on Restaurant Name</h5>
                     </div>
